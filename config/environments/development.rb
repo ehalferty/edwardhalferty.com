eae236bf6ff9c39eefb069494cbec712dcac8305
Edward Halferty <edhalferty@gmail.com>
2014-10-06 22:53:23 -0700

    Use memcached for session store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.cache_store = :dalli_store, 'localhost', { namespace: 'edwardhalferty.com', expires_in: 1.day, compress: true }
 end
